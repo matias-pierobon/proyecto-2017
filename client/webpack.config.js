f4ff1f1e6ba56e561c90525acf0f102157be6c02
Matías Pierobón Marcos <matias.pierobonm@gmail.com>
2017-08-30 17:13:04 -0300

    add clinet skeleton and webpack

---
+++ client/webpack.config.js
@@ -0,0 +1,44 @@
+const path = require("path")
+const CleanWebpackPlugin = require("clean-webpack-plugin")
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "build")
+  },
+  plugins: [new CleanWebpackPlugin(["build"])],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: "style-loader" // creates style nodes from JS strings
+          },
+          {
+            loader: "css-loader" // translates CSS into CommonJS
+          },
+          {
+            loader: "sass-loader" // compiles Sass to CSS
+          }
+        ]
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.(png|svg|jpg|gif|woff|woff2|eot|ttf|otf)$/,
+        use: ["file-loader"]
+      }
+    ]
+  }
+}
