8365be7c535d1c818fc0f04c58eff0157f65a8c7
Matias Pierobon <matias.pierobonm@gmail.com>
2017-09-30 17:56:20 -0300

    test spl

--- www/index.php
+++ www/index.php
@@ -6,4 +6,21 @@
  * Time: 2:18 PM
  */
 
-phpinfo();
+$s = new SplObjectStorage();
+
+$o1 = new StdClass;
+$o2 = new StdClass;
+$o3 = new StdClass;
+
+$s->attach($o1);
+$s->attach($o2);
+
+var_dump($s->contains($o1));
+var_dump($s->contains($o2));
+var_dump($s->contains($o3));
+
+$s->detach($o2);
+
+var_dump($s->contains($o1));
+var_dump($s->contains($o2));
+var_dump($s->contains($o3));
