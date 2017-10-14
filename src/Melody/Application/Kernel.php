49c58340675e7fde9db9bc606efb614030e3d9ba
Matías Pierobon <matias.pierobonm@gmail.com>
2017-10-13 23:18:03 -0300

    kernel scheleton

---
+++ src/Melody/Application/Kernel.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: matias
+ * Date: 10/13/17
+ * Time: 7:31 PM
+ */
+
+namespace Melody\Application;
+
+
+class Kernel {
+
+    public function handle($request)
+    {
+
+    }
+}
