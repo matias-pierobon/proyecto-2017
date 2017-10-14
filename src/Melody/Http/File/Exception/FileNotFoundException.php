b5abbabc16c1456a7a1e81171f23d9d372a40c8d
Matías Pierobon <matias.pierobonm@gmail.com>
2017-10-14 15:21:58 -0300

    Add base exception for files

---
+++ src/Melody/Http/File/Exception/FileNotFoundException.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: matias
+ * Date: 10/14/17
+ * Time: 12:19 PM
+ */
+
+namespace Melody\Http\File\Exception;
+
+
+class FileNotFoundException extends FileException
+{
+
+}
