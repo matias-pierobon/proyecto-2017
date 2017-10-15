8365b42414a326bee27c6e1e246a7c24ad0b8e80
Matías Pierobon <matias.pierobonm@gmail.com>
2017-10-15 16:10:15 -0300

    start base kernel

--- src/Melody/Application/Kernel.php
+++ src/Melody/Application/Kernel.php
@@ -8,11 +8,88 @@
 
 namespace Melody\Application;
 
+use Melody\Http\Request;
+use Melody\Http\Response;
+use Melody\Router\Match;
+use Melody\Router\Route;
+use Melody\Router\Router;
 
+/**
+ * Class Kernel
+ * @package Melody\Application
+ */
 class Kernel {
 
+    /* @var Application */
+    private $application;
+
+    /**
+     * @param Application $application
+     */
+    public function __construct($application)
+    {
+        $this->application = $application;
+    }
+
+    /**
+     * @param Request $request
+     */
     public function handle($request)
     {
+        /* @var Response|null $response */
+        $response = null;
+        try {
+            $response = $this->handleRaw($request);
+        }catch (\Exception $exception){
+            $response = $this->handleException($exception, $request);
+        }finally{
+            $response->send();
+        }
+    }
 
+    /**
+     * @param Request $request
+     * @return Response
+     */
+    private function handleRaw($request){
+        $route = $this->getRoute($request);
+        $ctrl = $this->getController($route->getController());
+        $action = $route->getAction() . 'Action';
+        return $ctrl->$action($request);
+    }
+
+    protected function getController($controller){
+        return $this->application->getController($controller);
+    }
+
+    /**
+     * @param Request $request
+     *
+     * @return Route
+     */
+    protected function getRoute($request){
+
+        $match = $this->matchRequest($request);
+        $request->setParameters($match->getParams());
+        return $match->getRoute();
+    }
+
+    /* @return Match */
+    protected function matchRequest($request){
+        return $this->getRouter()->matchRequest($request);
+    }
+
+    /* @return Router */
+    protected function getRouter(){
+        return $this->application->getRouter();
+    }
+
+    /**
+     * @param \Exception $exception
+     * @param Request $request
+     * @return Response
+     */
+    private function handleException($exception, $request){
+        return new Response($exception->getMessage(), $exception->getCode());
     }
 }
